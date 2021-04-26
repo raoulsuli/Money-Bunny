fb69136434e9b3c3a424189b06d6e27288a2e957
Raoul <sulimoviciraoul@yahoo.com>
2021-04-26 15:03:29 +0300

    express update

--- Money-Bunny/src/database.js
+++ Money-Bunny/src/database.js
@@ -1,4 +1,8 @@
-var mysql = require('mysql')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
 var connection = mysql.createConnection({
     host: 'money-bunny-instance-1.c8zbm7k90fa0.eu-north-1.rds.amazonaws.com',
     user: 'admin',
@@ -9,10 +13,12 @@ var connection = mysql.createConnection({
 
 connection.connect()
 
-connection.query('SELECT * FROM Tabela;', function(err, rows, fields) {
-    rows.forEach(function (row) {
-        console.log(JSON.parse(JSON.stringify(row)));
-    })
-})
+const app = express().use(cors()).use(bodyParser.json());
+
+// connection.query('SELECT * FROM Tabela;', function(err, rows, fields) {
+//     rows.forEach(function (row) {
+//         console.log(JSON.parse(JSON.stringify(row)));
+//     })
+// })
 
 connection.end()
