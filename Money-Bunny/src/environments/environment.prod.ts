cfe04ca565d068ec6b83f38a405761e019c5f333
Ioana Georgescu <ioana.gxgscu@gmail.com>
2021-04-30 12:05:39 +0300

    firebase in environment

--- Money-Bunny/src/environments/environment.prod.ts
+++ Money-Bunny/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCsHIiwhDalF7EzK94K5foxzhBnoQKxqP0",
+    authDomain: "money-bunny-920e7.firebaseapp.com",
+    databaseURL: "https://money-bunny-920e7-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "money-bunny-920e7",
+    storageBucket: "money-bunny-920e7.appspot.com",
+    messagingSenderId: "197205358903",
+    appId: "1:197205358903:web:83098ad642277dae9282b2",
+    measurementId: "G-ZLFV0BB7N5"
+  }
 };
--- Money-Bunny/src/environments/environment.ts
+++ Money-Bunny/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCsHIiwhDalF7EzK94K5foxzhBnoQKxqP0",
+    authDomain: "money-bunny-920e7.firebaseapp.com",
+    databaseURL: "https://money-bunny-920e7-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "money-bunny-920e7",
+    storageBucket: "money-bunny-920e7.appspot.com",
+    messagingSenderId: "197205358903",
+    appId: "1:197205358903:web:83098ad642277dae9282b2",
+    measurementId: "G-ZLFV0BB7N5"
+  }
 };
 
 /*
