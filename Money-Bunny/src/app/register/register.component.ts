ec3502e85990934c22e5a27c0f7456ad7c9de6df
Ioana Georgescu <ioana.gxgscu@gmail.com>
2021-05-07 11:59:33 +0300

    register

--- Money-Bunny/src/app/register/register.component.ts
+++ Money-Bunny/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from './../services/authentication.service';
 import { MoneyBunnyUser } from './../models/money-bunny-user.model';
+import { AngularFirestore } from "@angular/fire/firestore";
 
 @Component({
   selector: 'app-register',
@@ -15,31 +16,59 @@ export class RegisterComponent implements OnInit {
 		'', // email
 		'', // cnp
 		'', // phone
-		new Date(),
+		new Date(), // birthday
 		'', // address
 		'', // userType
-		undefined
+		'' // company name: firestore doesn't recognise undefined
   );
   passCheck: string = '';
+  invalidUser: string = '';
  
-  constructor(private authenticationService:AuthenticationService) { }
+  constructor(private authenticationService:AuthenticationService, private firestore: AngularFirestore) { }
 
   signUp() {
-    this.authenticationService.SignUp(this.newUser.email, this.newUser.password);
-  }
-  
-  signIn() {
-    this.authenticationService.SignIn(this.newUser.email, this.newUser.password);
-  }
-  
-  signOut() {
-    this.authenticationService.SignOut();
+    if (this.passCheck === this.newUser.password) {
+
+      var existentUser = this.firestore.collection('users').doc(this.newUser.username);
+
+      existentUser.get().toPromise().then((doc) => {
+        if (!doc.exists) {
+          this.authenticationService.SignUp(this.newUser.email, this.newUser.password);
+          this.firestore.collection('users').doc(this.newUser.username).set({
+            name: this.newUser.name,
+            username: this.newUser.username,
+            password: this.newUser.password,
+            email: this.newUser.email,
+            cnp: this.newUser.cnp,
+            phone: this.newUser.phone,
+            birthday: this.newUser.birthday,
+            address: this.newUser.address,
+            userType: this.newUser.userType,
+            companyName: this.newUser.companyName
+          })
+          .then(res => {
+              console.log(res);
+          })
+          .catch(e => {
+              console.log(e);
+          })
+        } else {
+          this.invalidUser = this.newUser.username;
+        }
+      }).catch((error) => {
+          console.log("Error getting document:", error);
+      });
+    }
   }
 
-  ngOnInit(): void {
+  usernameInUse() {
+    return this.invalidUser == this.newUser.username;
   }
 
   setUserType(event: Event) {
     this.newUser.userType = (<HTMLInputElement>event.target).value;
+  }
+
+  ngOnInit(): void {
   }
 }
