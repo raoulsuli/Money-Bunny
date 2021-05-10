beb9e373af02cd827d59fb34afb1838733df92a7
Raoul <sulimoviciraoul@yahoo.com>
2021-05-10 16:57:49 +0300

    Update to Transactions Report

--- Money-Bunny/src/app/transactions-report/transactions-report.component.ts
+++ Money-Bunny/src/app/transactions-report/transactions-report.component.ts
@@ -9,19 +9,27 @@ import { AuthenticationService } from '../services/authentication.service';
 })
 export class TransactionsReportComponent implements OnInit {
   public transactions: any = [] as any;
-  public currentAccount = this.auth.getCurrentUser();
+  public currentUser = this.auth.getCurrentUser();
+  public currentAccount: any;
   public all_accounts: any = [] as any;
 
   constructor(private firestore: AngularFirestore, private auth: AuthenticationService) {
     if (this.transactions.length != 0) return;
 
-    this.firestore.collection('accounts').valueChanges().subscribe((data: any) => this.all_accounts = data);
-    
+    this.firestore.collection('accounts').valueChanges().subscribe((data: any) => {
+      this.all_accounts = data;
+      data.forEach((res: any) => {
+        if (res['account_name'] == this.auth.getCurrentAccount()) {
+          this.currentAccount = res;
+        }
+      })
+    });
+
     this.firestore.collection('transactions').valueChanges().subscribe((data: any) => {
       data.forEach((element: any) => {
         element['IBAN_src'].get().then((result: any) => {
           result.data()['user_id'].get().then((res: any) => {
-            if (res.data()['username'] == this.currentAccount) {
+            if (res.data()['username'] == this.currentUser) {
               this.transactions.push(element);
             }
           })
