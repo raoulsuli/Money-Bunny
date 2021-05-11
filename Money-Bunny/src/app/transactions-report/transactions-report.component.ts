ed56abe8d1bc4531fdc34ef114b4da6f27cacf3a
Raoul <sulimoviciraoul@yahoo.com>
2021-05-11 22:52:57 +0300

    Transactions reports update and some other fixes

--- Money-Bunny/src/app/transactions-report/transactions-report.component.ts
+++ Money-Bunny/src/app/transactions-report/transactions-report.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Subscription } from 'rxjs';
+import { Transaction } from '../models/transaction.model';
 import { AuthenticationService } from '../services/authentication.service';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
 
 @Component({
   selector: 'app-transactions-report',
@@ -12,10 +16,9 @@ export class TransactionsReportComponent implements OnInit {
   public currentUser = this.auth.getCurrentUser();
   public currentAccount: any;
   public all_accounts: any = [] as any;
+  public subscription: Subscription;
 
   constructor(private firestore: AngularFirestore, private auth: AuthenticationService) {
-    if (this.transactions.length != 0) return;
-
     this.firestore.collection('accounts').valueChanges().subscribe((data: any) => {
       this.all_accounts = data;
       data.forEach((res: any) => {
@@ -25,25 +28,35 @@ export class TransactionsReportComponent implements OnInit {
       })
     });
 
-    this.firestore.collection('transactions').valueChanges().subscribe((data: any) => {
+    this.subscription = this.firestore.collection('transactions').valueChanges().subscribe((data: any) => {
       data.forEach((element: any) => {
         element['IBAN_src'].get().then((result: any) => {
           result.data()['user_id'].get().then((res: any) => {
-            if (res.data()['username'] == this.currentUser) {
+            if (res.data()['username'] == this.currentUser || element['IBAN_dest'] == this.currentAccount['IBAN']) {
+              element['IBAN_src'].get().then((acc: any) => {
+                element['account_iban'] = acc.data()['IBAN'];
+              });
               this.transactions.push(element);
+              this.transactions.sort((t1: Transaction, t2: Transaction) => (t1['date'] < t2['date'] ? 1 : -1));
             }
           })
         });
-        this.all_accounts.forEach((acc: any) => {
-          if (acc['IBAN'] == element['IBAN_dest']) {
-            this.transactions.push(element);
-          }
-        })
       });
+      this.unsubscribe();
     });
   }
 
   ngOnInit(): void {
+    
   }
 
+  downloadPDF() {
+    const doc = new jsPDF();
+    autoTable(doc, { html: '#transactions-table' });
+    doc.save('transactions.pdf');
+  }
+
+  unsubscribe(): void {
+    this.subscription.unsubscribe();
+  }
 }
