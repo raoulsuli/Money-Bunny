375385fa617cb24c6d8d15a17abf9e95bc0ae6d5
Raoul <sulimoviciraoul@yahoo.com>
2021-05-12 12:44:02 +0300

    Finished Transactions Reports

--- Money-Bunny/src/app/transactions-report/transactions-report.component.ts
+++ Money-Bunny/src/app/transactions-report/transactions-report.component.ts
@@ -17,8 +17,14 @@ export class TransactionsReportComponent implements OnInit {
   public currentAccount: any;
   public all_accounts: any = [] as any;
   public subscription: Subscription;
+  public filter: string = "today";
+  public dates: any = [] as any;
+  private date = new Date;
+  public months = ['January ', 'February ', 'March ', 'April ', 'May ', 'June ', 'July ', 'August ',
+                  'September ', 'October ', 'November ', 'December '];
 
   constructor(private firestore: AngularFirestore, private auth: AuthenticationService) {
+    for (var i = 0; i < this.months.length; i++) this.months[i] += this.date.getFullYear();
     this.firestore.collection('accounts').valueChanges().subscribe((data: any) => {
       this.all_accounts = data;
       data.forEach((res: any) => {
@@ -34,9 +40,12 @@ export class TransactionsReportComponent implements OnInit {
           result.data()['user_id'].get().then((res: any) => {
             if (res.data()['username'] == this.currentUser || element['IBAN_dest'] == this.currentAccount['IBAN']) {
               element['IBAN_src'].get().then((acc: any) => {
-                element['account_iban'] = acc.data()['IBAN'];
+                element['source_iban'] = acc.data()['IBAN'];
               });
               this.transactions.push(element);
+              if (!this.dates.includes(element['date'].toDate().getMonth()))
+                this.dates.push(element['date'].toDate().getMonth());
+                this.dates.sort((t1: number, t2: number) => (t1 > t2 ? 1 : -1));
               this.transactions.sort((t1: Transaction, t2: Transaction) => (t1['date'] < t2['date'] ? 1 : -1));
             }
           })
@@ -46,9 +55,7 @@ export class TransactionsReportComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void {}
 
   downloadPDF() {
     const doc = new jsPDF();
@@ -58,5 +65,14 @@ export class TransactionsReportComponent implements OnInit {
 
   unsubscribe(): void {
     this.subscription.unsubscribe();
+  }
+
+  getItems() {
+    if (this.filter == 'today') return this.transactions.filter((item: any) => item['date'].toDate().toDateString() === this.date.toDateString());
+    else if (this.filter == 'lastweek') {
+      var last_week = new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate() - 7);
+      return this.transactions.filter((item: any) => item['date'].toDate() <= this.date && item['date'].toDate() >= last_week)
+    }
+    return this.transactions.filter((item: any) => item['date'].toDate().getMonth().toString() == this.filter);
   }
 }
